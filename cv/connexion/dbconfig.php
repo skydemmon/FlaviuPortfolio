6183f9dc88a675b54460a9775829f17c213e8d00
skydemmon <skydemmon@gmail.com>
2020-12-10 15:17:46 +0100

    remove gitignore

--- cv/connexion/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostname = "localhost";
-$username = "pc3p9s04_wp482";
-$password = "Flaviu1999";
-$db = "pc3p9s04_wp482";
-?>
-<!-- site key=6LdObOQZAAAAAC1yTVKyjf-tiigljf5xTP6nVaKP
-secret=6LcCZ-QZAAAAAJW5tV7SS9Hj7rdnvjTVC5vjSmC- -->
